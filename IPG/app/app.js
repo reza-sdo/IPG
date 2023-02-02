922de482ddb3a2af0340b7b5030c6785720af285
Reza <rezamohammadzade939@gmail.com>
2023-02-02 16:41:49 +0330

    some details

--- IPG/app/app.js
+++ IPG/app/app.js
@@ -3,7 +3,7 @@ const totPrice = location.search.substring(1); //query
 const payBtns = document.querySelectorAll(".nav-li");
 const payPage = document.querySelectorAll(".pays");
 
-
+const cardNumber = document.querySelector("#cardNumber");
 
 const requestCode = document.querySelector("#code-2ver");
 
@@ -11,12 +11,12 @@ const price = document.querySelector("#price");
 const currency = document.querySelector("#currency");
 const submit = document.querySelector("#pay");
 
-const cardNumber = document.querySelector("#cardNumber");
-
 const closeBtn = document.querySelector("#closeCardNumBtn");
 
 const cardSaves = document.querySelector(".cardSaves");
 const savedCards = document.querySelector("#savedCards");
+
+const captchaImg = document.querySelector("#captcha-img");
 
 const showSavedCard = document.querySelector("#showSaveCard");
 
@@ -47,30 +47,34 @@ const finalContinueBtn = document.querySelector("#doneContinue");
 
 const forWether = document.getElementById("forWether");
 
-
-
 const cancel = document.querySelector("#cancell");
 
 const cardShowMonth = document.querySelector(".month-card-show");
 const cardShowYear = document.querySelector(".year-card-show");
 
+const crypToQrCode = document.querySelector("#crypToQrCode");
+const walletAddressToShow = document.querySelector(".wallet-addres");
 
-const crypToQrCode = document.querySelector("#crypToQrCode")
-const walletAddressToShow = document.querySelector(".wallet-addres")
+const mobileBankQr = document.querySelector(".mobile-qr");
+const mobileBtn = document.querySelector(".mobile-bank-open-btn");
 
+const webNameInAside = document.querySelector("#web-name");
 
-const mobileBankQr = document.querySelector(".mobile-qr")
-const mobileBtn = document.querySelector(".mobile-bank-open-btn")
+const trNo = document.querySelector(".trNo");
+const refNo = document.querySelector(".refNo");
 
-const webNameInAside = document.querySelector("#web-name")
-
-const trNo = document.querySelector(".trNo")
-const refNo = document.querySelector(".refNo")
-
+savedCardsHandler();
+showSavedCardBoxHandler();
 
 addDotIn3Digits();
 
-cryptoHandler()
+getBtcPrice(); // 100request in month  , To speed up the work
+
+cryptoHandler();
+
+const randNum8digits = () => {
+   return Math.floor(Math.random() * 100000000);
+};
 
 // all document number convert to persian : jquery
 
@@ -106,8 +110,6 @@ function ConvertNumberToPersion() {
    traverse(document.body);
 }
 
-
-
 // limit inputs to get just numbers : jquery
 
 $("#cardNumber,#cvv2,#exMonth,#exYear,#passwordInput,#userPhone").keypress(
@@ -116,13 +118,12 @@ $("#cardNumber,#cvv2,#exMonth,#exYear,#passwordInput,#userPhone").keypress(
    }
 );
 
+// add , in 3th digits in price
+function addDotIn3Digits() {
+   price.innerText = totPrice.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-
-
-
-
-
-// to make a persian numbers in inputs , Except captcha and secondPass 
+// to make a persian numbers in inputs , Except captcha and secondPass
 inputNumsToPersian(cardNumber, exMonth, exYear, phoneNumber);
 function inputNumsToPersian(...i) {
    i.forEach((j) => {
@@ -131,115 +132,7 @@ function inputNumsToPersian(...i) {
       });
    });
 }
-
-
-
-
-const randNumForRefTrNo = ()=>{
-   return Math.floor(Math.random() * 100000000)
-}
-
-
-
-function cryptoHandler(){
-   const walletAddress = "19DKsTY7XMThaoo2kNnspXNRBERUk4iLHB"
-   new QRCode(crypToQrCode , walletAddress)
-   walletAddressToShow.innerText = walletAddress
-   walletAddressToShow.addEventListener("click", copyText);
-         
-
-   // function for copying selected text in clipboard
-   function copyText() {
-      selectText();
-      document.execCommand("copy");
-   }
-
-
-   // function for selecting the text of an element based on the event.target (supporting IE)
-   function selectText() {
-      var element = event.target;
-      var range;
-      if (document.selection) {
-         // IE
-         range = document.body.createTextRange();
-         range.moveToElementText(element);
-         range.select();
-      } else if (window.getSelection) {
-         range = document.createRange();
-         range.selectNode(element);
-         window.getSelection().removeAllRanges();
-         window.getSelection().addRange(range);
-      }
-   }
-}
-
-mobileHandler()
-function mobileHandler() {
-   const address = "https://www.bki.ir/"
-   new QRCode(mobileBankQr, address);
-   mobileBtn.addEventListener("click" , ()=>{
-      location.href = address
-   })
-}
-
-
-
-
-function addDotIn3Digits() {
-   price.innerText = totPrice.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-savedCardsHandler();
-
-
-function savedCardsHandler() {
-   if (JSON.parse(localStorage.getItem("card")) == null){
-      return
-   }
-   const inLocal = JSON.parse(localStorage.getItem("card"));
-
-   inLocal.forEach((i) => {
-      let divs = document.createElement("div");
-      const cardnum = i.cardNum;
-      const cardMonth = i.cardMonth;
-      const cardYear = i.cardYear;
-      const logoSrc = i.logoSrc;
-      const fData = `<img src="${logoSrc}"/><span>${cardnum}</span><span>${cardYear}/${cardMonth}</span>`;
-      divs.innerHTML = fData;
-      savedCards.appendChild(divs);
-   });
-}
-
-showSavedCardHandler();
-
-function showSavedCardHandler() {
-   savedCards.childNodes.forEach((i) => {
-      i.addEventListener("click", (e) => {
-         console.log(e.currentTarget.childNodes[2]);
-         inputCardNumLogo.src = e.currentTarget.childNodes[0].src;
-
-         inputCardNumLogo.src,
-            (asideCardLogo.src = e.currentTarget.childNodes[0].src);
-         inputCardNumLogo.style.position = "relative";
-         inputCardNumLogo.style.visibility,
-            (asideCardLogo.style.visibility = "visible");
-
-         cardNumber.value = e.currentTarget.childNodes[1].innerText;
-         cardNumShow.innerText = e.currentTarget.childNodes[1].innerText;
-
-         exMonth.value = e.currentTarget.childNodes[2].innerText.split("/")[1];
-         exYear.value = e.currentTarget.childNodes[2].innerText.split("/")[0];
-
-         cardShowMonth.innerText =
-            e.currentTarget.childNodes[2].innerText.split("/")[1];
-         cardShowYear.innerText =
-            e.currentTarget.childNodes[2].innerText.split("/")[0];
-            showSavedCardBtnHandler()
-      });
-   });
-}
-
-console.log(typeof savedCards.children);
-// getBtcPrice()      // 100request in month
+getBtcPrice();
 function getBtcPrice() {
    const key = "freeLMrTTaNux2WcldNslnH0lksXx7F8";
    const url = `http://api.navasan.tech/latest/?api_key=${key}&item=btc`;
@@ -257,6 +150,89 @@ function test(i) {
    btc = totPrice / i;
 }
 
+// To place the saved cards in a special place , These cards come from local storage
+function savedCardsHandler() {
+   if (JSON.parse(localStorage.getItem("card")) == null) {
+      return;
+   }
+   const inLocal = JSON.parse(localStorage.getItem("card"));
+
+   inLocal.forEach((i) => {
+      let divs = document.createElement("div");
+      const cardnum = i.cardNum;
+      const cardMonth = i.cardMonth;
+      const cardYear = i.cardYear;
+      const logoSrc = i.logoSrc;
+      const fData = `<img src="${logoSrc}"/><span>${cardnum}</span><span>${cardYear}/${cardMonth}</span>`;
+      divs.innerHTML = fData;
+      savedCards.appendChild(divs);
+   });
+}
+
+function cryptoHandler() {
+   const walletAddress = "19DKsTY7XMThaoo2kNnspXNRBERUk4iLHB";
+   new QRCode(crypToQrCode, walletAddress);
+   walletAddressToShow.innerText = walletAddress;
+   walletAddressToShow.addEventListener("click", copyText);
+}
+// function for copying selected text in clipboard
+function copyText() {
+   selectText();
+   document.execCommand("copy");
+}
+
+// function for selecting the text of an element based on the event.target (supporting IE)
+function selectText() {
+   var element = event.target;
+   var range;
+   if (document.selection) {
+      // IE
+      range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+   } else if (window.getSelection) {
+      range = document.createRange();
+      range.selectNode(element);
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+   }
+}
+
+mobileHandler();
+function mobileHandler() {
+   const address = "https://www.bki.ir/";
+   new QRCode(mobileBankQr, address);
+   mobileBtn.addEventListener("click", () => {
+      location.href = address;
+   });
+}
+
+function showSavedCardBoxHandler() {
+   savedCards.childNodes.forEach((i) => {
+      i.addEventListener("click", (e) => {
+         console.log(e.currentTarget.childNodes[2]);
+         inputCardNumLogo.src = e.currentTarget.childNodes[0].src;
+
+         asideCardLogo.src = e.currentTarget.childNodes[0].src;
+         inputCardNumLogo.style.position = "relative";
+         inputCardNumLogo.style.visibility = "visible";
+         asideCardLogo.style.visibility = "visible";
+
+         cardNumber.value = e.currentTarget.childNodes[1].innerText;
+         cardNumShow.innerText = e.currentTarget.childNodes[1].innerText;
+
+         exMonth.value = e.currentTarget.childNodes[2].innerText.split("/")[1];
+         exYear.value = e.currentTarget.childNodes[2].innerText.split("/")[0];
+
+         cardShowMonth.innerText =
+            e.currentTarget.childNodes[2].innerText.split("/")[1];
+         cardShowYear.innerText =
+            e.currentTarget.childNodes[2].innerText.split("/")[0];
+         showSavedCardBtnHandler();
+      });
+   });
+}
+
 /////////////////////////////////////////////////////////
 
 function saveCardInLocal() {
@@ -271,19 +247,18 @@ function saveCardInLocal() {
       b.push(a);
       localStorage.setItem("card", JSON.stringify(b));
    } else {
-      inLocal.forEach((i)=>{
-         if(i.cardNum == cardNum && i.cardMonth == cardMonth && i.cardYear == cardYear){
-            return
+      for (i of inLocal) {
+         if (i.cardNum == cardNum) {
+            return;
          }
-         inLocal.push(a);
-         localStorage.setItem("card", JSON.stringify(inLocal));
-      })
+      }
+      inLocal.push(a);
+      localStorage.setItem("card", JSON.stringify(inLocal));
    }
 }
 
-////////////////////////////////////////////////////////////////////////
+// page timer
 pageTimer();
-
 function pageTimer() {
    const sec1 = document.querySelector(".sec-1");
    const sec2 = document.querySelector(".sec-2");
@@ -316,12 +291,15 @@ function pageTimer() {
    }, 1000);
 }
 
-function back() {}
+// captcha refresh btn
 document.querySelector("#captchaRef").addEventListener("click", (e) => {
-   e.target.style.transform == "rotate(180deg)"?e.target.style.transform = "rotate(360deg)":e.target.style.transform = "rotate(180deg)"
+   e.target.style.transform == "rotate(180deg)"
+      ? (e.target.style.transform = "rotate(360deg)")
+      : (e.target.style.transform = "rotate(180deg)");
    makeCaptcha();
 });
 
+//ex year handler in card show , add 0 to 1 digits
 exYear.addEventListener("change", () => {
    const m = perNumToEn(exYear);
    if (m.length == 1) {
@@ -330,7 +308,6 @@ exYear.addEventListener("change", () => {
       exYear.value = `۰۰`;
    }
 });
-
 exYear.addEventListener("keyup", () => {
    cardShowYear.innerHTML = exYear.value;
 
@@ -340,6 +317,8 @@ exYear.addEventListener("keyup", () => {
       cardShowYear.innerHTML = `۰۰`;
    }
 });
+
+//ex month handler in card show , add 0 to 1 digits , month most smaller than 12
 
 exMonth.addEventListener("change", (e) => {
    const m = perNumToEn(exMonth);
@@ -356,7 +335,6 @@ exMonth.addEventListener("change", (e) => {
       exMonth.value = `۰۰`;
    }
 });
-
 exMonth.addEventListener("keyup", () => {
    cardShowMonth.innerHTML = exMonth.value;
 
@@ -367,20 +345,20 @@ exMonth.addEventListener("keyup", () => {
    }
 });
 
+// cvv2 should be more than 3 digits
+
 cvv2.addEventListener("change", () => {
    if (cvv2.value.length < 3) {
       cvv2Error.style.display = "inline-block";
-      if (cvv2.value.length == 0) {
-         cvv2Error.style.display = "none";
-      }
    } else {
       cvv2Error.style.display = "none";
    }
 });
 
-showSavedCard.addEventListener("click", showSavedCardBtnHandler)
+// to rotate btn for shows save cards and show save cards btn
+showSavedCard.addEventListener("click", showSavedCardBtnHandler);
 
-function showSavedCardBtnHandler(){
+function showSavedCardBtnHandler() {
    if (showSavedCard.style.transform === "rotate(180deg)") {
       cardSaves.style.display = "none";
       showSavedCard.style.transform = "rotate(0deg)";
@@ -390,8 +368,7 @@ function showSavedCardBtnHandler(){
    }
 }
 
-const captchaImg = document.querySelector("#captcha-img");
-
+// to clear card number input and rest card show number(aside)
 document.querySelector("#closeCardNumBtn").addEventListener("click", () => {
    cardNumber.value = "";
    inputCardNumLogo.style.visibility = "hidden";
@@ -400,10 +377,25 @@ document.querySelector("#closeCardNumBtn").addEventListener("click", () => {
    cardNumShow.innerHTML = PersianTools.digitsEnToFa("0000 0000 0000 0000");
 });
 
+//to add space beetwin 4 digits
+const formatCardNumber = (number) =>
+   number.split("").reduce((seed, next, index) => {
+      if (index !== 0 && !(index % 4)) seed += " ";
+      return seed + next;
+   }, "");
+cardNumber.addEventListener("input", () => {
+   cardNumber.value = formatCardNumber(cardNumber.value.replaceAll(" ", ""));
+   bankLogoHandeler();
+   cardNumShow.innerHTML == ""
+      ? (cardNumShow.innerHTML = "0000 0000 0000 0000")
+      : (cardNumShow.innerHTML = cardNumber.value);
+});
 
+function perNumToEn(i) {
+   return PersianTools.digitsFaToEn(i.value.split(" ").join(""));
+}
 
-// add space in 4th index
-
+// banks logo and card numbers
 const bankLogos = {
    610433: "mellat",
    589905: "melli",
@@ -451,19 +443,6 @@ const bankLogos = {
    639599: "ghavamin",
 };
 
-cardNumber.addEventListener("input", (e) => {
-   cardNumber.value = formatCardNumber(cardNumber.value.replaceAll(" ", ""));
-   bankLogoHandeler();
-   // cardNumShow.innerHTML=cardNumber.value
-   cardNumShow.innerHTML == ""
-      ? (cardNumShow.innerHTML = "0000 0000 0000 0000")
-      : (cardNumShow.innerHTML = cardNumber.value);
-});
-
-function perNumToEn(i) {
-   return PersianTools.digitsFaToEn(i.value.split(" ").join(""));
-}
-
 function bankLogoHandeler() {
    let convertedCardNum = perNumToEn(cardNumber);
 
@@ -483,13 +462,6 @@ function bankLogoHandeler() {
       }
    }
 }
-
-
-const formatCardNumber = (number) =>
-   number.split("").reduce((seed, next, index) => {
-      if (index !== 0 && !(index % 4)) seed += " ";
-      return seed + next;
-   }, "");
 
 // to make captcha
 let vC;
@@ -512,26 +484,11 @@ const makeCaptcha = () => {
 };
 makeCaptcha();
 
-let smsCode;
-requestCode.addEventListener("click", (e) => {
-   e.preventDefault();
-   if (
-      cardNumCheck() &&
-      cvv2Check() &&
-      monthAndYearCheck() &&
-      cpatCheck(vC) &&
-      phonCheck()
-   ) {
-      console.log("okookoko");
-      requestBtnActiveTimerHandler();
-      smsCode = sendSms();
-   }
-});
-
+// to disable btns
 const disableBtn = (i) => {
    requestCode.disabled = i;
 };
-
+// change request code btn to disable
 function requestBtnActiveTimerHandler() {
    requestCode.style.backgroundColor = "#DDDDDD";
    requestCode.style.color = "black";
@@ -545,6 +502,7 @@ function requestBtnActiveTimerHandler() {
    console.log("in");
    disableBtn(true);
 }
+// change request code btn to enable
 function disableTimerRequestBtn() {
    requestCode.style.backgroundColor = "#4C9E3A";
    requestCode.style.color = "#fff";
@@ -558,6 +516,7 @@ function disableTimerRequestBtn() {
    return;
 }
 
+// request btn timer
 function reversTimer(ele) {
    let time = PersianTools.digitsFaToEn(ele.innerText);
    let min = +time.split(":")[0];
@@ -583,6 +542,8 @@ function reversTimer(ele) {
       )} : ${PersianTools.digitsEnToFa(sec)}`;
    }, 1000);
 }
+
+/////////////////////////// checkers to request btn and final pay btn
 
 function cardNumCheck() {
    if (cardNumber.value.length < 19 || cardNumError.style.display === "block") {
@@ -641,8 +602,6 @@ function phonCheck() {
    }
 }
 
-//secondPassInp
-
 function secPassCheck() {
    if (secondPassInp.value == smsCode) {
       document.querySelector(".passError").style.display = "none";
@@ -653,6 +612,8 @@ function secPassCheck() {
    }
 }
 
+// to send sms   note : To receive SMS, you must not turn off the advertising SMS of your SIM card!!
+
 function sendSms() {
    if (!phonCheck()) return;
    const phonNum = PersianTools.digitsFaToEn(phoneNumber.value);
@@ -660,18 +621,37 @@ function sendSms() {
    const apiKey =
       "KU6dFkAF6bQMBZYsY17PJfzhzuFs6bQSePMcq18BW9cU0Li7BiLKhBRSVOJvbkYM";
    const line = 30007732007385;
-   const code = Math.floor(Math.random() * 100000000);
-   console.log(code);
+   const code = randNum8digits();
+   console.log(code); // if you don't receive the sms
    const text = `رمز شما:  code:${code}`;
-
    const url = `https://api.sms.ir/v1/send?username=${userName}&password=${apiKey}&line=${line}&mobile=${phonNum}&text=${text}`;
 
    // fetch(url)
    //    .then((res) => res.json())
-   //    .then((data) => console.log(data));
+   //    .then((data) => console.log(data)); // come back data
 
    return code;
 }
+
+// request btn handler
+
+let smsCode;
+requestCode.addEventListener("click", (e) => {
+   e.preventDefault();
+   if (
+      cardNumCheck() &&
+      cvv2Check() &&
+      monthAndYearCheck() &&
+      cpatCheck(vC) &&
+      phonCheck()
+   ) {
+      console.log("okookoko");
+      requestBtnActiveTimerHandler();
+      smsCode = sendSms();
+   }
+});
+
+// to change nav btns
 
 const priceSave = price.innerText;
 payBtns.forEach((i) => {
@@ -683,6 +663,7 @@ payBtns.forEach((i) => {
    });
 });
 
+//to show currency of each tab
 function currencyHandler(ev) {
    const target = ev.target.id;
    payPage.forEach((i) => {
@@ -701,7 +682,6 @@ function currencyHandler(ev) {
    });
 }
 
-
 function removeActiveBtns() {
    payBtns.forEach((i) => {
       i.classList.remove("activebtns");
@@ -714,6 +694,7 @@ function removeActiveContainer() {
    });
 }
 
+// final submit btn handler
 submit.addEventListener("click", () => {
    if (
       cardNumCheck() &&
@@ -733,6 +714,7 @@ submit.addEventListener("click", () => {
    }
 });
 
+// final data to send backend
 const finalData = () => {
    return {
       cardNumber: cardNumber.value,
@@ -743,20 +725,21 @@ const finalData = () => {
    };
 };
 
+// for done page
 function finalHandeler() {
-
-
    console.log(finalData(), "we can fetch this data");
    document.querySelector(".main-main").style.display = "none";
    document.querySelector(".done").style.display = "flex";
    document.querySelector(".footer-elements").style.display = "none";
    document.querySelector(".footer-continue").style.display = "flex";
-   trNo.innerText = `شماره پیگیری :  ${randNumForRefTrNo()}`
-   refNo.innerText = `شماره مرجع :  ${randNumForRefTrNo()}`
+   trNo.innerText = `شماره پیگیری :  ${randNum8digits()}`;
+   refNo.innerText = `شماره مرجع :  ${randNum8digits()}`;
 }
 function finalBtn() {
    finalContinueBtn.addEventListener("click", backToShopOk);
 }
+
+// for show tehrans wether
 function wether() {
    const key = "f2bbc52e18cabb66a5da6dacf22dda03";
 
@@ -771,10 +754,13 @@ function wether() {
          forWether.innerHTML = ` دمای هوای تهران :${temp}  `;
       });
 }
+
+// If the payment was successful
 function backToShopOk() {
    location.href = "../index.html?200";
 }
 
+// If the payment was not successful and cancel btn
 function backToShopNotOk() {
    Swal.fire({
       title: "آیا مطمئن هستید؟",
@@ -789,6 +775,8 @@ function backToShopNotOk() {
       }
    });
 }
+
+//If the payment time is over
 
 function timeUp() {
    Swal.fire({
